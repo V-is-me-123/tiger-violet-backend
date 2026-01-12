f0dae475b1776e9e14d333ea92ca7bdb8c6f2958
V-is-me-123 <violet.orlinski@gmail.com>
2026-01-12 14:37:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def create_checkout_session():
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=["card"],
             line_items=[{
-                "price": PRICE_IDS.[product_id],
+                "price": PRICE_IDS[product_id],
                 "quantity": 1
             }],
             mode="payment",
