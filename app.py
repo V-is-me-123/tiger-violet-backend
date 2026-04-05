e8d4ea7a8a18b9271cc598b9ce49500cf48e5515
V-is-me-123 <violet.orlinski@gmail.com>
2026-04-05 18:56:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,10 @@ def success():
 def cancel():
     return "<h1>Payment canceled. You can try again.</h1>"
 
+@app.route("/ping")
+def ping():
+    return "ok", 200
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
